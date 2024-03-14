7b1771b209e15ad92cfbba1ad77fcc0d6bf63972
Diego Brito Sánchez <diegobritosanchez13@gmail.com>
2024-03-14 09:25:22 +0000

    Base config

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dq9tz6ypn*%v&@!(ex9q@2vc(atqx+dl1k+ie$l!ism(@-(t)u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 
